12e4705fb00b0ce87b6200b5bcd604aa3345800d
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-11-01 20:30:19 +0100

    add function to delete colonn in the db file

--- Read_db_file_sqlite.py
+++ Read_db_file_sqlite.py
@@ -4,29 +4,36 @@
  			- Dmonstrate SQL to fetch data 
  			- Select and update data of the SQL table 
 '''  
+  '''
+ Main task :
+ 			- Readsql filein python
+ 			- Dmonstrate SQL to fetch data 
+ 			- Select and update data of the SQL table 
+'''  
   
 # importing the module 
 import sqlite3 
   
 # connect withe the myTable database 
 connection = sqlite3.connect("sample.db") 
-  
-# cursor object 
-crsr = connection.cursor() 
 
-#update the vaue of age as fucntion of the id 
-crsr.execute("UPDATE lesemployers SET age = 30 WHERE id =1")
-  
-# execute the command to fetch all the data from the table emp 
-#SELECT * FROM employees;
-crsr.execute("SELECT * FROM lesemployers")  
-  
-# store all the fetched data in the ans variable 
-ans= crsr.fetchall()  
-  
-# loop to print all the data 
-for i in ans: 
-    print(i) 
+def read_all_task(connection):
+    # cursor object 
+    crsr = connection.cursor() 
+
+    #update the value of age as fucntion of the id 
+    crsr.execute("UPDATE lesemployers SET age = 30 WHERE id =1")
+
+    # execute the command to fetch all the data from the table emp 
+    #SELECT * FROM employees;
+    crsr.execute("SELECT * FROM lesemployers")  
+      
+    # store all the fetched data in the ans variable 
+    ans= crsr.fetchall()  
+      
+    # loop to print all the data 
+    for i in ans: 
+        print(i) 
 
 
 def select_task_by_age(connection, age):
@@ -46,9 +53,38 @@ def select_task_by_age(connection, age):
         print(row)
         filtred_list.append(row)
 
-    print("he filtred list by age is :",filtred_list)
+    print("the filtred list by age is :",filtred_list)
     #return filtred_list
- 
-#try 
-print('The selction of the employees by a defiend age is :')
-select_task_by_age(connection, age=28)
+
+# add delete function 
+
+#Delete based on a character 
+'''##Delete based on a character 
+
+DELETE FROM employees WHERE job_title IS NULL;
+SELECT * FROM employees;'''
+
+def delete_task_by_job(connection, age):
+
+	cur = connection.cursor()
+	cur.execute("DELETE FROM lesemployers WHERE age = ?",(age,))
+
+	rows=cur.fetchall()
+
+	for row in rows:
+		print(row)
+
+
+if __name__ =='__main__':
+
+
+    print("#------------------#")
+    delete_task_by_job(connection, age = 63)
+
+    print('The all task in je bdd file are :')
+    read_all_task(connection)
+
+    #try the function 
+
+    print('The selction of the employees by a defiend age is :')
+    select_task_by_age(connection, age=28)
