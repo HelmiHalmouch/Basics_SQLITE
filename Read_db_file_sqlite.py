c4de95895e222119a9f4fa821d8fc06636dbe695
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-11-01 18:50:36 +0100

    read the sql table and fetch the data

--- Read_db_file_sqlite.py
+++ Read_db_file_sqlite.py
@@ -9,3 +9,17 @@ import sqlite3
  
 # connect withe the myTable database 
 connection = sqlite3.connect("sampleTable.db") 
+
+# cursor object 
+crsr = connection.cursor() 
+  
+# execute the command to fetch all the data from the table emp 
+crsr.execute("SELECT * FROM lesemplyers")  
+  
+# store all the fetched data in the ans variable 
+ans= crsr.fetchall()  
+  
+# loop to print all the data 
+for i in ans: 
+    print(i) 
+
